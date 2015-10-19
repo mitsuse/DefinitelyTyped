0a383868f3210e022a2f3ae365a3fc9ccbc04124
Tomoya Kose <tomoya@mitsuse.jp>
2015-10-19 21:34:52 +0900

    Write the header of d.ts.

---
+++ instagram-node/instagram-0.5.8.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for instagram-node v0.5.8
+// Project: https://github.com/totemstech/instagram-node
+// Definitions by: Tomoya Kose <https://github.com/mitsuse>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
